3ccade9e6c77a52132305d9ce2430dbb17f974d7
Vanesa Porcelli <vanesaporcelli@gmail.com>
2023-07-14 17:33:57 -0300

    se quita variables de entorno por errores

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import{getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROYECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING,
-    appId: import.meta.env.VITE_APP_ID,
+    apiKey: "AIzaSyDIIb8enTl5PS7bCRoDmYv7ihKSNJs-NTg",
+    authDomain: "tukura-fdac0.firebaseapp.com",
+    projectId: "tukura-fdac0",
+    storageBucket: "tukura-fdac0.appspot.com",
+    messagingSenderId: "418322039797",
+    appId: "1:418322039797:web:c949f143ee9b4a364dd6bc"
 };
 
 const app = initializeApp(firebaseConfig);
